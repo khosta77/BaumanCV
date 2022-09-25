2c0099b2ef6d43ff78dc3c8b5c0954eb9e294f22
khosta77 <puwerfulpants@gmail.com>
2022-09-25 18:10:35 +0300

    Сокращено время работы цикла.

--- project/include/correlation.h
+++ project/include/correlation.h
@@ -34,16 +34,35 @@ namespace correlation {
          /** \arithmetic_mean - возращает среднее значение
           * */
          inline void arithmetic_mean(float &m_, const Mat &M) const noexcept {
-             for (size_t i = 0; i < M.size(); ++i)
+             for (size_t i = 0; i < M.size() - 10; i+=10) { // Все размеры снимков кратны 10
                  m_ += M[i];
+                 m_ += M[i + 1];
+                 m_ += M[i + 2];
+                 m_ += M[i + 3];
+                 m_ += M[i + 4];
+                 m_ += M[i + 5];
+                 m_ += M[i + 6];
+                 m_ += M[i + 7];
+                 m_ += M[i + 8];
+                 m_ += M[i + 9];
+             }
              m_ /= M.size();
          }
 
         /** \mean_square_deviation - один из знаминателей в формуле Пирсона
          * */
         void mean_square_deviation(const Mat &M, const float &m_, float &Rm) const noexcept {
-            for (size_t i = 0; i < M.size(); ++i) {
+            for (size_t i = 0; i < M.size() - 10; i+=10) {
                 Rm += ((M[i] - m_) * (M[i] - m_));
+                Rm += ((M[i + 1] - m_) * (M[i + 1] - m_));
+                Rm += ((M[i + 2] - m_) * (M[i + 2] - m_));
+                Rm += ((M[i + 3] - m_) * (M[i + 3] - m_));
+                Rm += ((M[i + 4] - m_) * (M[i + 4] - m_));
+                Rm += ((M[i + 5] - m_) * (M[i + 5] - m_));
+                Rm += ((M[i + 6] - m_) * (M[i + 6] - m_));
+                Rm += ((M[i + 7] - m_) * (M[i + 7] - m_));
+                Rm += ((M[i + 8] - m_) * (M[i + 8] - m_));
+                Rm += ((M[i + 9] - m_) * (M[i + 9] - m_));
             }
             Rm = std::sqrt(Rm);
         }
@@ -51,8 +70,17 @@ namespace correlation {
         /** \mean_square_deviation - вычисляет числитель из формулы Пирсона
          * */
         inline void mean_square_deviation(const Mat &M, const Mat &N) noexcept {
-            for (size_t i = 0; i < M.size(); ++i) {
+            for (size_t i = 0; i < M.size() - 10; i+=10) {
                 Rmn += ((M[i] - Rm_) * (N[i] - Rn_));
+                Rmn += ((M[i + 1] - Rm_) * (N[i + 1] - Rn_));
+                Rmn += ((M[i + 2] - Rm_) * (N[i + 2] - Rn_));
+                Rmn += ((M[i + 3] - Rm_) * (N[i + 3] - Rn_));
+                Rmn += ((M[i + 4] - Rm_) * (N[i + 4] - Rn_));
+                Rmn += ((M[i + 5] - Rm_) * (M[i + 5] - Rn_));
+                Rmn += ((M[i + 6] - Rm_) * (M[i + 6] - Rn_));
+                Rmn += ((M[i + 7] - Rm_) * (M[i + 7] - Rn_));
+                Rmn += ((M[i + 8] - Rm_) * (M[i + 8] - Rn_));
+                Rmn += ((M[i + 9] - Rm_) * (M[i + 9] - Rn_));
             }
         }
     };
